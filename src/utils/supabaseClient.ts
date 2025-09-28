c74c150056b7a6743f03d0f37081f1df13a7fc70
MHgh0st <mhgh0st.982@gmail.com>
2025-09-28 21:18:08 +0330

    just configs

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // خواندن متغیرها از فایل .env.local
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY!;
 
 // ایجاد و اکسپورت کردن کلاینت Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
